0b6884c47b47f832be9c081b8b4e4857d7b2ee6e
Redseb <mikolajzyzanski@gmail.com>
2021-10-28 11:04:01 +0200

    Changed music script to be run withing the App component, added better mobile styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import music from './mixdown_1.mp3'
-import './musicScript'
+import music from './assets/music/mixdown_1.mp3'
+// import './musicScript'
 
 function App() {
+  let audio = new Audio(music);
+  const startMusic = () => {
+    const playPromise = audio.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(_ => {
+          console.log("audio played auto");
+        })
+        .catch(error => {
+          console.log("playback prevented");
+        });
+    }
+  }
   return (
     <div className="mainContainer">
-      <h1><span id="Logo"><span id="logoRed">sick</span><span id="logoWhite">.</span><span id="logoGreen">cash</span></span></h1>
+      <h1 onClick={startMusic}><span id="Logo"><span id="logoRed">sick</span><span id="logoWhite">.</span><span id="logoGreen">cash</span></span></h1>
       <h2>Coming Soon...</h2>
-      <audio id="mixdown" autoplay loop><source src={music}/></audio>
     </div>
   );
-  }
+}
 
 export default App;
--- src/musicScript.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    var song = document.getElementById("mixdown");
-    var logo = document.getElementById("Logo");
-    logo.onclick = function() {
-        song.play();
-    }
-}
