cf3f4f73236d9737759215daa0bb0421b7e97897
Redseb <mikolajzyzanski@gmail.com>
2022-01-31 19:36:20 +0100

    Updated placeholder site with background component from react-tsparticles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import music from './assets/music/mixdown_1.mp3'
 // import './musicScript'
+import Particles from "react-tsparticles";
 
 function App() {
   let audio = new Audio(music);
@@ -18,6 +19,78 @@ function App() {
   }
   return (
     <div className="mainContainer">
+      <Particles
+        id="tsparticles"
+        options={{
+          fpsLimit: 120,
+          background: {
+            color: {
+              value: "#403d3c",
+            },
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "cover",
+          },
+          fullScreen: {
+            zIndex: -1,
+          },
+          particles: {
+            move: {
+              enable: true,
+              outModes: {
+                bottom: "out",
+                left: "out",
+                right: "out",
+                top: "out",
+              },
+              direction: "bottom",
+              angle: 20,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 15,
+            },
+            rotate: {
+              random: {
+                enable: true,
+              },
+              animation: {
+                enable: true,
+                speed: 1,
+              },
+              direction: "random",
+            },
+            color: {
+              value: "#ffffff",
+            },
+            opacity: {
+              anim: { enable: true, opacity_min: 0.1, speed: 0.1, sync: true },
+              random: false,
+              value: 0.5,
+            },
+            shape: {
+              options: {
+                char: {
+                  fill: true,
+                  font: "Font Awesome 5 Free",
+                  value: [
+                    "\uf1fe", //chart-area
+                    "\uf080", //chart-bar
+                    "\uf201", //chart-line-up
+                  ],
+                  weight: "900",
+                },
+              },
+              type: "char",
+            },
+            size: {
+              value: 32,
+            },
+          },
+        }}
+      />
       <h1 onClick={startMusic}><span id="Logo"><span id="logoRed">sick</span><span id="logoWhite">.</span><span id="logoGreen">cash</span></span></h1>
       <h2>Coming Soon...</h2>
     </div>
